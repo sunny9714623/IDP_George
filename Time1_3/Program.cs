77995791564ba9bd71fc5ffb6b6afb3216ca0907
George <971462342@qq.com>
2021-12-21 20:34:35 +0800

    update

--- Time1_3/Program.cs
+++ Time1_3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using static System.Console;
 using System.Linq;
 
@@ -8,7 +8,7 @@ namespace Time1_3
     {
         static void Main(string[] args)
         {
-            int[] nums1 = new int[] { 9, 9, 9,9,9,9,9 };
+            int[] nums1 = new int[] { 9,9,9,9,9,9,9 };
             ListNode l1 = CreateList(nums1);
             int[] nums2 = new int[] { 9, 9, 9,9 };
             ListNode l2 = CreateList(nums2);
@@ -40,43 +40,24 @@ namespace Time1_3
             ListNode l3 = null;
             ListNode l3Back = null;
             int add = 0;
-            while (l1!= null && l2!= null && add !=0)
+            while (l1!= null || l2!= null || add!=0)
             {
-                int val = l1.val + l2.val + add;
-                if (val > 9)
-                {
-                    add = 1;
-                }
-                else
-                {
-                    add = 0;
-                }
+                add = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + add;
                 if (l3 == null)
                 {
-                    l3 = new ListNode();
-                    l3.val = val % 10;
+                    l3 = new ListNode(add % 10);
+                    add /= 10;
                     l3.next = null;
                     l3Back = l3;
                 }
                 else
                 {
-                    l3Back.next = new ListNode(val % 10);
+                    l3Back.next = new ListNode(add % 10);
+                    add /= 10;
                     l3Back = l3Back.next;
                 }
-                l1 = l1.next;
-                l2 = l2.next;
-                if (l2 == null)
-                {
-                    l2.val = 0;
-                }
-            }
-            if (l1 == null)
-            {
-                l3Back.next = l2;
-            }
-            else
-            {
-                l3Back.next = l1;
+                l1 = l1 ?.next;
+                l2 = l2 ?.next;
             }
             return l3;
         }
