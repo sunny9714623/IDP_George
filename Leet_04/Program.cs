277888aea18fe302d3b0df2513cdfbdf3f3e5995
GeorgeL <971462342@qq.com>
2021-12-22 19:19:35 +0800

    12/22's work

---
+++ Leet_04/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Leet_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[] { };
+            int[] nums2 = new int[] { 1 };
+            Console.WriteLine(FindMedianSortedArrays(nums1, nums2));
+        }
+        // O(m+n)
+        public static double FindMedianSortedArrays(int[] nums1,int[] nums2)
+        {
+            int median = nums1.Length + nums2.Length;
+            int left = 0, right = 0, idx = 0;
+            int[] nums = new int[(nums1.Length + nums2.Length) / 2+1];
+            while (idx < nums.Length)
+            {
+                if (left < nums1.Length && right < nums2.Length)
+                {
+                    nums[idx++] = nums1[left] < nums2[right] ? nums1[left++] : nums2[right++];
+                }
+                else if (left >= nums1.Length)
+                {
+                    nums[idx++] = nums2[right++];
+                }
+                else
+                {
+                    nums[idx++] = nums1[left++];
+                }
+            }
+            return median % 2 == 0 ? (double)(nums[nums.Length - 1] + nums[nums.Length - 2]) / 2 : (double)nums[nums.Length - 1];
+        }
+
+        // O(log(m+n)
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+
+        }
+        public int getNumberOfK(int[] nums1,int[] nums2,int k)
+        {
+            int length1 = nums1.Length, length2 = nums2.Length;
+            int half = k / 2;
+            if (length1 == 0)
+            {
+                return nums2[k];
+            }
+            if (length2 == 0)
+            {
+                return nums1[k];
+            }
+            while (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ Time1_4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Time1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "pwwkew";
+            Console.WriteLine(LengthOfLongestSubstring(s));
+        }
+        //public static int LengthOfLongestSubstring(string s)
+        //{
+        //    int max = 0;
+        //    char[] str = s.ToCharArray();
+        //    System.Collections.Generic.List<char> chin = new System.Collections.Generic.List<char>();
+        //    foreach(char ch in str)
+        //    {
+        //        if (chin.Contains(ch))
+        //        {
+        //            max = max > chin.Count ? max : chin.Count;
+        //            chin.Add(ch);
+        //            chin = chin.GetRange(chin.IndexOf(ch)+1, chin.Count - chin.IndexOf(ch) - 1);
+        //        }
+        //        else
+        //        {
+        //            chin.Add(ch);
+        //        }
+        //    }
+        //    max = max > chin.Count ? max : chin.Count;
+        //    return max;
+        //}
+
+        // 滑动窗口
+        public static int LengthOfLongestSubstring (string s)
+        {
+            int left = 0, right = 0,max=0;
+            System.Collections.Generic.List<char> chin = new System.Collections.Generic.List<char>();
+            while (right < s.Length)
+            {
+                if (!chin.Contains(s[right]))
+                {
+                    chin.Add(s[right++]);
+                }
+                else
+                {
+                    chin.Remove(s[left++]);
+                }
+                max = max > right - left ? max : right - left ;
+            }
+            return max;
+        }
+    }
+}
