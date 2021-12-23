61bf3cd9ad6202e8a185190cb236a8fa8b62e3d3
GeorgeL <971462342@qq.com>
2021-12-23 19:59:33 +0800

    fix

---
+++ Leet_05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leet_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "cbbd";
+            Console.WriteLine(LongestPlaindrome(s));
+        }
+        public static string LongestPlaindrome(string s)
+        {
+            int left = 0, right = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                extend(s, i, i,ref left,ref right);
+                extend(s, i, i + 1, ref left, ref right);
+            }
+            return s.Substring(left, right-left+1);
+        }
+        // 两个指针，从中间往两边走，一边走一边扩展，如果相等就判断是否是长度满足。
+        public static void extend(string s,int i,int j,ref int left,ref int right)
+        {
+            while (i >= 0 && j < s.Length && j - i + 1 <= s.Length && s[i] == s[j])
+            {
+                if (right - left < j - i)
+                {
+                    right = j;
+                    left = i;
+                }
+                j++;
+                i--;
+            }
+        }
+    }
+}
---
+++ Leet_06/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leet_06
+{
+    class Program
+    {
+        /// <summary>
+        /// 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 N 字形排列。
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Convert("PAYPALISHIRING", 3));
+        }
+        public static string Convert(string s,int numRows)
+        {
+            if (numRows == 0)
+            {
+                return s;
+            }
+            string ret="";
+            string[] rows = new string[numRows];
+            char[] str = s.ToCharArray();
+            int curRow = 0;
+            bool down = false;
+            foreach (char c in str)
+            {
+                if (curRow == 0 || curRow == numRows - 1)
+                {
+                    rows[curRow] += c;
+                    down = !down;
+                    curRow += down ? 1 : -1;
+                }
+                else
+                {
+                    rows[curRow] += c;
+                    curRow += down ? 1 : -1;
+                }
+            }
+            foreach(string ls in rows)
+            {
+                ret += ls;
+            }
+            return ret;
+        }
+    }
+}
