5c931e0596a66a4cc530369de4775c009c770c9d
George <971462342@qq.com>
2022-01-06 22:57:01 +0800

    leet_11

---
+++ Leet_11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Leet_11
+{
+    /// <summary>
+    /// 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。
+    /// 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            Console.WriteLine(MaxArea(test));
+        }
+        public static int MaxArea(int[] height)
+        {
+            int left = 0, right = height.Length-1;
+            int result = 0;
+            while (left < right)
+            {
+                result = Math.Max(result, (right - left) * Math.Min(height[left], height[right]));
+                if (height[left] < height[right]) left++;
+                else right--;
+            }
+            return result;
+        }
+    }
+}
