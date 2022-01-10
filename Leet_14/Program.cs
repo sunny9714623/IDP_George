b6652dc7e8c3224b132f24d31a40765578fa178c
GeorgeL <971462342@qq.com>
2022-01-10 13:53:31 +0800

    Leet 14

--- Leet_14/Program.cs
+++ Leet_14/Program.cs
@@ -2,11 +2,36 @@
 
 namespace Leet_14
 {
+    /// <summary>
+    /// 编写一个函数来查找字符串数组中的最长公共前缀。
+    /// 如果不存在公共前缀，返回空字符串 ""。
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            
+            string[] strs = new string[] { "dog", "racecar", "car" };
+            string s = LongestCommonPrefix(strs);
+            Console.WriteLine(s);
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            string s = "";
+            int i = 0;
+            while (i < strs[0].Length)
+            {
+                s += strs[0][i];
+                foreach (string str in strs)
+                {
+                    if (!str.StartsWith(s))
+                    {
+                        s = s.Remove(s.Length - 1);
+                    }
+                }
+                i++;
+            }
+            return s;
         }
     }
 }
