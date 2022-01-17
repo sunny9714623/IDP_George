699623241fdf9d95ec9138665a193c3a579b5a14
George <971462342@qq.com>
2022-01-17 22:57:53 +0800

    力扣18提

--- Leet_18/Program.cs
+++ Leet_18/Program.cs
@@ -16,32 +16,29 @@ namespace Leet_18
         {
             var ans = new List<List<int>>();
             nums = nums.OrderBy(t => t).ToArray();
-            bool flag = true;
             for(int i = 0; i < nums.Length; i++)
             {
                 /// 去重
                 if (i > 0 && nums[i] == nums[i - 1]) continue;
+                if ((long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+                if ((long)nums[i] + nums[nums.Length - 3] + nums[nums.Length - 2] + nums[nums.Length - 1] < target) continue;
                 for (int j = i + 1; j < nums.Length; j++)
                 {
                     /// 去重
                     if (j > i+1 && nums[j] == nums[j - 1])
                     {
-                        if (flag)
-                        {
-                            flag = false;
-                        }
-                        else
-                        {
-                            continue;
-                        }
+                        continue;
                     }
-                    for(int k = j + 1, l = nums.Length - 1; k < l;)
+                    if ((long)nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+                    if ((long)nums[i] + nums[j] + nums[nums.Length - 2] + nums[nums.Length - 1] < target) continue;
+                    for (int k = j + 1, l = nums.Length - 1; k < l;)
                     {
-                        int total = nums[i] + nums[j] + nums[k] + nums[l];
+                        long total = nums[i] + nums[j] + nums[k] + nums[l];
                         if (total == target)
                         {
                             ans.Add(new List<int> { nums[i] , nums[j] , nums[k] , nums[l] });
                             k++;l--;
+                            // 去重
                             while (k < l && (nums[k] == nums[k - 1])) k++;
                             while (k < l && (nums[l] == nums[l + 1])) l--;
                         }
