cb8b2c98f97256bbc22997aa1e015f73223e57d8
GeorgeL <971462342@qq.com>
2022-02-10 18:02:05 +0800

    30

---
+++ Leet_30/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Leet_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public IList<int> FindSubstring(string s,string[] words)
+        {
+
+            return new List<int>();
+        }
+    }
+}
