2c7b611172b198a86620cc90b5e3af58b8c7d690
GeorgeL <971462342@qq.com>
2022-02-16 18:35:18 +0800

    update

---
+++ Leet_33/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Leet_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static int Serch(int[] nums,int target)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target) return i;
+            }
+            return -1;
+        }
+    }
+}
