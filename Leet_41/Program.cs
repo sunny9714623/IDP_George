2c39de3c5766ce96c26dbcb637a8b7f65e713518
GeorgeL <971462342@qq.com>
2022-02-21 18:19:57 +0800

    update

--- Leet_41/Program.cs
+++ Leet_41/Program.cs
@@ -1,31 +1,39 @@
 ﻿using System;
-
+/// <summary>
+/// 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+/// 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+/// </summary>
 namespace Leet_41
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] { 2, 4, 5, 6, 1, 10 };
+            int[] nums = new int[] { 2, 4, 5, 6, 1, 10};
             int ret = FirstMissingPositive(nums);
         }
+        /// <summary>
+        /// 第一遍for循环，将正数归位。
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
         public static int FirstMissingPositive(int[] nums)
         {
             for(int i = 0; i < nums.Length; i++)
             {
-                if (nums[i] > 0 && nums[i] < nums.Length)
+                while (nums[i] > 0 && nums[i] <= nums.Length && nums[nums[i]-1] != nums[i])
                 {
-                    (nums[nums[i]]) = (nums[i]);
+                    (nums[i], nums[nums[i] - 1]) = (nums[nums[i] - 1], nums[i]); // 一遍交换可能没有归位，要使用while
                 }
             }
             for(int i = 0; i < nums.Length; i++)
             {
-                if (i != nums[i])
+                if (i+1 != nums[i])
                 {
-                    return i;
+                    return i+1;
                 }
             }
-            return nums.Length;
+            return nums.Length + 1;
         }
     }
 }
---
+++ Leet_42/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Leet_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            int ret = Trap(height);
+        }
+        public static int Trap(int[] height)
+        {
+            int ans = 0;
+            for(int i = 0; i < height.Length - 1; i++)
+            {
+                int leftmax = 0, rightmax = 0;
+                for (int j = i; j < height.Length; j++)
+                {
+                    rightmax = Math.Max(rightmax, height[j]);
+                }
+                for(int j = i; j >= 0; j--)
+                {
+                    leftmax = Math.Max(leftmax, height[j]);
+                }
+                ans += Math.Min(leftmax, rightmax) - height[i];
+            }
+            return ans;
+        }
+    }
+}
