e11b397c9a36154af521048b888336c2dca0262b
GeorgeL <971462342@qq.com>
2022-03-21 17:24:18 +0800

    test generic

---
+++ GenericTest0321/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GenericTest0321
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Test();
+            Console.WriteLine(test.WriteToConsole(1));
+            Console.WriteLine(test.WriteToConsole("asd"));
+            Console.WriteLine("*********");
+            Console.WriteLine(test.WriteToConsole<int>(1));
+            Console.WriteLine(test.WriteToConsole<string>("dasdsa"));
+        }
+
+    }
+    public class Test
+    {
+        public string WriteToConsole<T>(T t)
+        {
+            return (t.GetType().ToString());
+        }
+    }
+}
