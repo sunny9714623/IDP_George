c72dd762f590e08db55ad5af59864c3cf203e0cb
GeorgeL <971462342@qq.com>
2022-03-23 09:23:39 +0800

    update

--- AttributeTest/Program.cs
+++ AttributeTest/Program.cs
@@ -11,6 +11,7 @@ namespace AttributeTest
             Console.WriteLine(memberInfo1.Name);
             TypeInfo typeInfo = typeof(MyAttributeTest).GetTypeInfo();
             Console.WriteLine(typeInfo.FullName);
+            var myatbut = new MyAttributeTest(2,100);
             var test = typeInfo.GetCustomAttributes(typeof(MyAttribute), true);
             foreach (var item in test)
             {
@@ -20,15 +21,27 @@ namespace AttributeTest
             MemberInfo[] memberInfos = typeof(MyAttributeTest).GetMembers();
             MethodInfo[] methodInfos = typeof(MyAttributeTest).GetMethods();
             FieldInfo[] fieldInfos = typeof(MyAttributeTest).GetFields();
-            PropertyInfo[] propertyInfos = typeof(MyAttributeTest).GetProperties();
+            PropertyInfo[] propertyInfos = myatbut.GetType().GetProperties();
             foreach (var item in propertyInfos)
             {
+                Console.WriteLine("******* property.Name *******");
+                Console.WriteLine(item.Name);
+                Console.WriteLine("******* property.Name *******");
                 if ((item.GetCustomAttribute(typeof(MyAttribute), true)) is MyAttribute)
                 {
                     var test1 = (item.GetCustomAttribute(typeof(MyAttribute), true)) as MyAttribute;
                     Console.WriteLine(item.Name);
                     Console.WriteLine(test1.Test);
                 }
+                Console.WriteLine("****************");
+                Console.WriteLine(item.GetValue(myatbut,null));
+                Console.WriteLine("****************");
+            }
+            foreach (var item in fieldInfos)
+            {
+                Console.WriteLine("****************");
+                Console.WriteLine(item.GetValue(myatbut));
+                Console.WriteLine("****************");
             }
             Console.WriteLine("***********");
             TypeInfo typeInfo1 = typeof(MyAttributeTest1).GetTypeInfo();
@@ -54,15 +67,22 @@ namespace AttributeTest
     public class MyAttributeTest
     {
         public int CardId;
-        private Guid Guiid;
+        private Guid Guiid => Guid.NewGuid();
         [My("property_attribute")]
         public int IdNumber { get; set; }
-
+        public MyAttributeTest(int cardId,int IdNumber)
+        {
+            this.CardId = cardId;
+            this.IdNumber = IdNumber;
+        }
     }
 
     public class MyAttributeTest1 : MyAttributeTest
     {
+        public MyAttributeTest1(int id,int idnum) : base(id, idnum)
+        {
 
+        }
     }
     // 定义可以Usage的地方
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property,Inherited = true,AllowMultiple = true)]
