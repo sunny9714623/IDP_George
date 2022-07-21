96a40c83ce653062fc5197e0e7bd0b323f7b3675
george <971462342@qq.com>
2022-07-21 23:44:16 +0800

    update

---
+++ Leet_53/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Leet_53
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            int ret = MaxSubArray(ints);
+        }
+
+        /// <summary>
+        /// 这个思想好好理解下，动态规划。
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int MaxSubArray(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+            int max = nums[0], pre = 0;
+            foreach (int num in nums)
+            {
+                pre = Math.Max(num, pre + num);
+                max = Math.Max(pre, max);
+            }
+            return max;
+        }
+    }
+}
---
+++ Leet_55/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Leet_55
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 3, 2, 1, 8, 0, 1 };
+            var ret = CanJump(nums);
+        }
+
+        /// <summary>
+        /// 从后往前遍历，排除最后一个(最后一个可以为0),如果发现有0的值，就判断有没有前面某个索引处的值加索引大于这个0的索引，
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        //public static bool CanJump(int[] nums)
+        // {
+        //    if (nums == null || nums.Length == 1)
+        //    {
+        //        return true;
+        //    }
+        //    for(int i = nums.Length - 2; i >= 0; i--)
+        //    {
+        //        if (nums[i]==0)
+        //        {
+        //            int j;
+        //            for(j = i - 1; j >= 0; j--)
+        //            {
+        //                if (nums[j] + j > i) break;
+        //            }
+        //            if (j == -1)
+        //            {
+        //                return false;
+        //            }
+        //        }
+        //    }
+        //    return true;
+        //}
+
+
+        /// 贪心算法
+        public static bool CanJump(int[] nums)
+        {
+            if (nums == null || nums.Length == 1)
+            {
+                return true;
+            }
+            int length = nums.Length - 1;
+            int rightMax = 0;// 目前最远可达的位置
+            for(int i = 0; i < length; i++)
+            {
+                if (i <= rightMax)
+                {
+                    rightMax = Math.Max(rightMax, i + nums[i]);
+                }
+                if (rightMax >= length) return true;
+            }
+            return false;
+        }
+    }
+}
