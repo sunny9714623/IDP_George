264cc885ecc4020c191625461df5c3daba0dc733
GeorgeL <971462342@qq.com>
2022-07-26 16:35:21 +0800

    力扣51

--- Leet_51/Program.cs
+++ Leet_51/Program.cs
@@ -7,37 +7,71 @@ namespace Leet_51
     {
         static void Main(string[] args)
         {
-            
+            var ret = SolveNQueens(4); 
         }
         public static IList<IList<string>> SolveNQueens(int n)
         {
-            List<List<string>> ret = new List<List<string>>();
-            var chs = new char[n, n];
+            List<IList<string>> ret = new List<IList<string>>();
+            var chs = new char[n][];
+            for(int i = 0; i < n; i++)
+            {
+                chs[i] = new char[n];
+            }
             // initial chs
             for(int i = 0; i < n; i++)
             {
                 for(int j = 0; j < n; j++)
                 {
-                    chs[i, j] = '.';
+                    chs[i][j] = '.';
                 }
             }
-            return null;
+            PutQueen(chs, 0, ret);
+            return ret;
+        }
+
+        public static IList<string> CharToListString(char[][] chs)
+        {
+            var ret = new List<string>();
+            for(int i = 0; i < chs.Length; i++)
+            {
+                ret.Add(new String(chs[i]));
+            }
+            return ret;
+        } 
+
+        public static void PutQueen(char[][] chs,int row, List<IList<string>> ret)
+        {
+            if(row == chs.Length)
+            {
+                ret.Add(CharToListString(chs));
+                return;
+            }
+            for(int i = 0; i < chs.Length; i++)
+            {
+                if (IsValid(chs, i, row))
+                {
+                    chs[i][row] = 'Q';
+                    PutQueen(chs, row + 1, ret);
+                    chs[i][row] = '.';
+                }
+            }
         }
 
         /// <summary>
-        /// 判断chs的i行j列放皇后是否合法，如果不在同一列并且
+        /// 判断chs的k行l列放皇后是否合法，如果不在同一列并且
         /// </summary>
         /// <param name="chs"></param>
         /// <param name="j"></param>
         /// <param name="k"></param>
         /// <returns></returns>
-        public bool IsValid(char[,] chs,int k,int l)
+        public static bool IsValid(char[][] chs,int k,int l)
         {
             for(int i = 0; i < chs.Length; i++)
             {
                 for(int j = 0; j < chs.Length; j++)
                 {
-
+                    if (chs[i][j] == 'Q' && (i == k || Math.Abs(i - k) == Math.Abs(j - l)))
+                        return false;
                 }
             }
             return true;
